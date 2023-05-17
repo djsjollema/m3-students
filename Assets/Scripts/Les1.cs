bf1d35d1006cb9356f60150dea644f39c9ebb3d1
Jelle Sjollema <j.sjollema@ma-web.nl>
2023-05-17 13:13:52 +0200

    init

---
+++ Assets/Scripts/Les1.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Les1 : MonoBehaviour
+{
+    public GameObject A;
+    public GameObject B;
+    public Vector3 v;
+
+    public GameObject P;
+    float t = 0;
+    public float tmax = 0;
+    public float speed = 2.0f;
+
+    void Start()
+    {
+        P.transform.position = A.transform.position;
+    }
+
+    void Update()
+    {
+        v = B.transform.position - A.transform.position;
+        v = v.normalized;
+        P.transform.position += v*speed * Time.deltaTime;
+
+        float distance = (B.transform .position - P.transform.position).magnitude;
+        if(distance < (v * Time.deltaTime).magnitude)
+        {
+            P.transform.position = A.transform.position;
+            t = 0;
+        }
+        t += Time.deltaTime;
+        tmax = Mathf.Max(t, tmax);
+        
+    }
+}
